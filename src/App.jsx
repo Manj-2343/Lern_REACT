59076f62e26b268c5b0df3930cb9f60054946f84
Manoj Biswal <manojbiswal521@gmail.com>
2024-12-28 03:31:35 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React, { useReducer } from "react";
-const initialState = { count: 0 };
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { ...state, count: state.count + 1 };
-    case "decrement":
-      return { ...state, count: state.count - 1 };
-    case "reset":
-      return { ...state, count: 0 };
-    default:
-      return state;
-  }
-};
+import { initialState, reducer } from "./component/useReducer/reducer";
+
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <h1>{state.count}</h1>
+      <h1>Count:{state.count}</h1>
       <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
-      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 };
